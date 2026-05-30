905ad7c2f6cbc8f1be1e0e61a55855585e6bd53a
abhi2003vaish <abhivaish2003@gmail.com>
2026-05-30 21:15:49 +0530

    8.2-(Spring profile)

--- src/main/java/com/example/TestingApp/TestingAppApplication.java
+++ src/main/java/com/example/TestingApp/TestingAppApplication.java
@@ -1,12 +1,30 @@
 package com.example.TestingApp;
 
+import com.example.TestingApp.services.DataService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestingAppApplication {
+@RequiredArgsConstructor
+public class TestingAppApplication implements CommandLineRunner {
+
+	private final DataService dataService;
+
+	@Value("${my.variable}")
+	private String myVariable;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("my variable:"+myVariable);
+
+		System.out.println("the data is: "+dataService.getData());
 	}
 }
---
+++ src/main/java/com/example/TestingApp/services/DataService.java
@@ -0,0 +1,5 @@
+package com.example.TestingApp.services;
+
+public interface DataService {
+    String getData();
+}
